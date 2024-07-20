fd449d7b41d59bce84d30e60d7b29441fbcb7132
mikepreston17 <michael.n.preston@gmail.com>
2024-07-20 15:10:07 -0500

    testing yarn in nixpacks.toml

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./Pages/**/*.*{html,js}"],
-    theme: {
-        extend: {},
+  content: ["./Pages/**/*.*{html,js}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      purple: "#3f3cbb",
+      midnight: "#121063",
+      metal: "#565584",
+      tahiti: "#3ab7bf",
+      silver: "#ecebff",
+      "bubble-gum": "#ff77e9",
+      bermuda: "#78dcca",
     },
-    plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-    ],
-}
-
+  },
+  plugins: [require("tailwindcss"), require("autoprefixer")],
+};
